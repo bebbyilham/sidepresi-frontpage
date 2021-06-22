d84077a3c48cbde8cea02f2e16c07a247b86c7a1
bebbyilham <bebbyilham@gmail.com>
2021-06-22 15:52:09 +0700

    220621 1552

--- src/parts/ListBlogs/RenderItem.js
+++ src/parts/ListBlogs/RenderItem.js
@@ -3,6 +3,7 @@ import React from "react";
 import Link from "next/link";
 
 import IconPlay from "public/images/icon-play.svg";
+import { data } from "autoprefixer";
 
 export default function RenderItem({ item }) {
   console.log(item);
@@ -10,8 +11,21 @@ export default function RenderItem({ item }) {
     <div className="w-1/4 px-4 ">
       <div className="item">
         <figure className="item-image">
-          <img src="" alt="" />
+          <IconPlay></IconPlay>
+          <img
+            src={item.thumbnail ?? ""}
+            alt={item?.name ?? "some information"}
+          />
         </figure>
+        <div className="item-meta">
+          <h4 className="text-lg text-gray-900">{item?.name ?? "Blog name"}</h4>
+          <h5 className="text-lg text-gray-900">
+            {item?.category ?? "Blog name"}
+          </h5>
+        </div>
+        <Link href="/blogs/[slug]" as={`/blogs/${item.id}`}>
+          <a href="" className="link-wrapped"></a>
+        </Link>
       </div>
     </div>
   );
