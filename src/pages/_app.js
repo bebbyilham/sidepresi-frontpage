6a772aa7667b528e9f3d74c6168c65881c2bc535
bebbyilham <bebbyilham@gmail.com>
2021-07-29 11:48:26 +0700

    290721 1147 NProgress uninstall + env

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,15 @@
-import moduleName, { Router } from "next/router";
+// import Router from "next/router";
 import { ToastContainer } from "react-toastify";
-import NProgress from "nprogress";
-import "nprogress/nprogress.css";
+// import NProgress from "nprogress";
+// import "nprogress/nprogress.css";
 
 import "../../tailwindcss/style.css";
 import "react-toastify/dist/ReactToastify.css";
 
-NProgress.configure({ showSpinner: false });
-Router.events.on("routeChangeStart", () => NProgress.start());
-Router.events.on("routeChangeComplate", () => NProgress.done());
-Router.events.on("routeChangeError", () => NProgress.done());
+// NProgress.configure({ showSpinner: false });
+// Router.events.on("routeChangeStart", () => NProgress.start());
+// Router.events.on("routeChangeComplate", () => NProgress.done());
+// Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
--- src/parts/Header.js
+++ src/parts/Header.js
@@ -59,7 +59,7 @@ export default function Header({ onLight }) {
           </Link>
         </li>
         <li>
-          <Link href="/">
+          <Link href="/about">
             <a
               className={[
                 linkColor,
